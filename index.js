0dc212f5e37f62d49fa48b18d4ac2262d0fc11ed
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 11:23:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var express = require('express')
-var app = express()
+var http = require('http');
+var fs = require('fs');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+      var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});

7b30965fd4a342a2fb15f99f41715d3203c91a83
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 16:45:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express()
 var fs = require('fs');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
- 
+
 var template = {
   HTML:function(title, list, body, control){
     return `
@@ -34,7 +34,19 @@ var template = {
 }
 
 app.set('port', (process.env.PORT || 5000))
-
+app.get('/', function(request, response) { 
+  fs.readdir('./data', function(error, filelist){
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    ); 
+    response.send(html);
+  });
+});
+ 
 app.get('/page/:pageId', function(request, response) { 
   fs.readdir('./data', function(error, filelist){
     var filteredId = path.parse(request.params.pageId).base;
@@ -58,7 +70,7 @@ app.get('/page/:pageId', function(request, response) {
     });
   });
 });
-
+ 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })

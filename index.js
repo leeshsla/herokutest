bac101c78e9094525686e75c249e63173c5176c4
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 11:35:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
-var express = require('express')
-var app = express()
-var fs = require('fs');
-var template = require('./lib/template.js');
+const express = require('express')
+const app = express()
  
 app.set('port', (process.env.PORT || 5000))
-//route, routing
-//app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', function(request, response) { 
-  fs.readdir('./data', function(error, filelist){
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    ); 
-    response.send(html);
-  });
-});
- 
-app.get('/page', function(req, res) { 
-  return res.send('/page');
-});
+app.get('/', (req, res) => res.send('Hello World!'))
  
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))

98addfc2fb5906f51f12c166cb0510ca625a71d2
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 16:20:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
  
 var template = {
   HTML:function(title, list, body, control){
@@ -23,7 +25,7 @@ var template = {
     var list = '<ul>';
     var i = 0;
     while(i < filelist.length){
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      list = list + `<li><a href="/page/${filelist[i]}">${filelist[i]}</a></li>`;
       i = i + 1;
     }
     list = list+'</ul>';
@@ -46,8 +48,28 @@ app.get('/', function(request, response) {
   });
 });
  
-app.get('/page', function(req, res) { 
-  return res.send('/page');
+app.get('/page/:pageId', function(request, response) { 
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
+  });
 });
  
 app.listen(app.get('port'), function() {

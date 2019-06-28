de405075241d34c7dfc11db7bb835b0e9b7e5d54
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 11:29:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-var http = require('http');
+var express = require('express')
+var app = express()
 var fs = require('fs');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-      var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+var template = require('./lib/template.js');
+ 
+app.set('port', (process.env.PORT || 5000))
+//route, routing
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(request, response) { 
+  fs.readdir('./data', function(error, filelist){
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    ); 
+    response.send(html);
+  });
 });
-
+ 
+app.get('/page', function(req, res) { 
+  return res.send('/page');
+});
+ 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-});
+})

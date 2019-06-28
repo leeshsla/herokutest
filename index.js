12ba8e947a21e4c75602dfa28e605ba1f40baf1d
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 11:38:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
  
 app.set('port', (process.env.PORT || 5000))
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', function(req, res) { 
+  return res.send('/');
+});
+ 
+app.get('/page', function(req, res) { 
+  return res.send('/page');
+});
  
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))

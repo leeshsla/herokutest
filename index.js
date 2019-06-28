ae83bb70b22dea125d5070c6c0ac140927ea2e02
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 11:20:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-var http = require('http');
-var fs = require('fs');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-      var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
-});
+  response.send('Hello World!')
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-});
+})

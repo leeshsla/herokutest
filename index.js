afe9d9a7eb48586f95c090331704fbffa24094fc
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 16:15:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
-var path = require('path');
-var qs = require('querystring');
-var sanitizeHtml = require('sanitize-html');
  
 var template = {
   HTML:function(title, list, body, control){
@@ -49,63 +46,8 @@ app.get('/', function(request, response) {
   });
 });
  
-app.get('/page/:pageId', function(request, response) { 
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      });
-      var list = template.list(filelist);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update?id=${sanitizedTitle}">update</a>
-          <form action="delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html);
-    });
-  });
-});
- 
-app.get('/create', function(request, response){
-  fs.readdir('./data', function(error, filelist){
-    var title = 'WEB - create';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list, `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.send(html);
-  });
-});
- 
-app.post('/create_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `/?id=${title}`});
-        response.end();
-      })
-  });
+app.get('/page', function(req, res) { 
+  return res.send('/page');
 });
  
 app.listen(app.get('port'), function() {

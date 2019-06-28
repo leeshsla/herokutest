5b2c23245f1c8365b659644e5cd184bcac06a0b1
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 15:30:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,98 +1,15 @@
 var express = require('express')
 var app = express()
-var http = require('http');
+var url = request.url;
 var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
 
-function templateHTML(title, list, body){
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    <a href="/create">create</a>
-    ${body}
-  </body>
-  </html>
-  `;
-}
-function templateList(filelist){
-  var list = '<ul>';
-  var i = 0;
-  while(i < filelist.length){
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i + 1;
-  }
-  list = list+'</ul>';
-  return list;
-}
- 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(req, res) { 
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-      if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-            response.send(template);
-        });
-      } else {
-        fs.readdir('./data', function(error, filelist){
-          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-            var title = queryData.id;
-            var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-            response.send(template);
-          });
-        });
-      }
+  url = '/index.html';
+  return res.send(fs.readFileSync(__dirname + url));
 });
  
-app.get('/create_process', function(req, res) { 
-        var body = '';
-      request.on('data', function(data){
-          body += data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var title = post.title;
-          var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.send();
-          })
-      });
-});
-
-app.get('/create', function(req, res) { 
-        fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = templateList(filelist);
-        var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `);
-        response.writeHead(200);
-        response.end(template);
-      });
-});
-
 app.get('/page', function(req, res) { 
   return res.send('/page');
 });

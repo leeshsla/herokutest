26815fe89f84dc3a7804c9884b946fcda81a6536
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 11:06:45 +0900

    1
    1

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+ 
+});
+app.set('port', (process.env.PORT || 5000))
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})

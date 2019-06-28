c8e97516a87ae5012e688f22d64b6d544739e4d4
leeshsla <52264542+leeshsla@users.noreply.github.com>
2019-06-28 15:08:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const express = require('express')
 const app = express()
- 
+var fs = require('fs');
+
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <a href="/create">create</a>
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list+'</ul>';
+  return list;
+}
+
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/', function(req, res) { 
-  return res.send('/');
+app.get('/', function(request, response) { 
+        fs.readdir('./data', function(error, filelist){
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            response.send(template);
+          });
+        });
 });
  
 app.get('/page', function(req, res) { 
